5924be45fadef326c62b66c426ac60f824e3178d
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-08 21:35:38 +0200

    08-06-2022 21:35

--- spring-boot-image-uploader/src/main/java/com/example/demo/WebSecurityConfig.java
+++ spring-boot-image-uploader/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 
 }
