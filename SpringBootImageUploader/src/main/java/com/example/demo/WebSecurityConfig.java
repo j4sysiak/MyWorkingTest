d5ff93e4c275bf5c87da4fef8e8be44ddb83d979
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-13 21:43:11 +0200

    13-06-2022 06:57

--- SpringBootImageUploader/src/main/java/com/example/demo/WebSecurityConfig.java
+++ SpringBootImageUploader/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -3,17 +3,26 @@ package com.example.demo;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collections;
+import java.util.Map;
 
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //zarządzanie dostępami z poziomu bazy danych
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication().withUser(new User("Jan", "Jan123", Collections.singleton(new SimpleGrantedAuthority("user"))));  //String username, String password, Collection<? extends GrantedAuthority> authorities
     }
 
+    //zarządzanie dostępami z poziomu http
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests().antMatchers("/test1").authenticated()
+                .and()
+                .formLogin().permitAll();
     }
 }
