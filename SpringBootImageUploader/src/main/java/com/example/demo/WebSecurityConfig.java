c025fd1064276aeee9ca4ab29a3c78c91c1d0a5b
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-13 22:17:29 +0200

    13-06-2022 06:57

--- SpringBootImageUploader/src/main/java/com/example/demo/WebSecurityConfig.java
+++ SpringBootImageUploader/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,21 +1,26 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
-import java.util.Map;
 
-
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     //zarządzanie dostępami z poziomu bazy danych
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(new User("Jan", "Jan123", Collections.singleton(new SimpleGrantedAuthority("user"))));  //String username, String password, Collection<? extends GrantedAuthority> authorities
+        auth.inMemoryAuthentication().withUser(
+                new User("xxx", passwordEncoder().encode("xxx"), Collections.singleton(new SimpleGrantedAuthority("user"))));
+        //String username, String password, Collection<? extends GrantedAuthority> authorities
     }
 
     //zarządzanie dostępami z poziomu http
@@ -24,5 +29,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/test1").authenticated()
                 .and()
                 .formLogin().permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
