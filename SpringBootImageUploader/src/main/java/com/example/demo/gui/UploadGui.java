fa73d83e1435ad70f3d6e4d3346a5ab63b16fff3
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-14 19:53:37 +0200

    14-06-2022 05:43

---
+++ SpringBootImageUploader/src/main/java/com/example/demo/gui/UploadGui.java
@@ -0,0 +1,11 @@
+package com.example.demo.gui;
+
+
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+@Route
+public class UploadGui extends VerticalLayout {
+
+
+}
