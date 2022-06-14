0e13a6a00c3c0d3224471aa5fc245a34babe3116
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-14 07:32:26 +0200

    14-06-2022 05:43

---
+++ SpringBootImageUploader/src/main/java/com/example/demo/ImageTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class ImageTest {
+
+    public static void main(String[] args) throws IOException {
+
+        Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+                        "cloud_name", "dmzun82tu",
+                        "api_key", "934522816349346",
+                        "api_secret", "1GwL921hBbmyJzR79Eoj9oQwBF8"));
+
+        File file = new File("smile.jpg");
+        Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+
+    }
+}
