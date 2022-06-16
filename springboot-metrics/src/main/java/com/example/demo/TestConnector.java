ff46cc66088e7bcc31d55e66f588c99b57a76068
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-16 19:37:20 +0200

    16-6-2022 17:11

--- springboot-metrics/src/main/java/com/example/demo/TestConnector.java
+++ springboot-metrics/src/main/java/com/example/demo/TestConnector.java
@@ -15,21 +15,27 @@ import java.util.Map;
 @Endpoint(id="test-connector")
 public class TestConnector {
 
-    @ReadOperation
+     @ReadOperation
     public Map<String, String> get() {
         Map<String, String> myMap = new HashMap();
-        RestTemplate restTemplate = new RestTemplate();
+         RestTemplate restTemplate = new RestTemplate();
         try {
             restTemplate.exchange("http://localhost:8081/test1", HttpMethod.GET, HttpEntity.EMPTY, String.class);
             //jeżeli uda się połączyć zewnętrzną aplikację - to zwracamy success
             //uruchamiamy zewnętrzy Rest: apka jest w: c:\Users\Jacek\Documents\JAVA\SpringBoot\MyWorkingTest\   działa na porcie 8081
-            //
+            //uruchom go przy użyciu jara (najpierw go wygeneruj w target a potem uruchom)
+//            2.sposób:
+//            $ ./mvnw package           (rapper mavena, można też użyć ./mvnw install)
+//            $ cd  target
+//            Jacek@BERLIN MINGW64 /C/Users/Jacek/git/Local_05-spring-boot-properties-demo/05-spring-boot-properties-demo/target (master)
+//            $ java -jar 05-spring-boot-properties-demo-0.0.1-SNAPSHOT.jar
+//            $ java -jar rest-test-0.0.1-SNAPSHOT.jar
             myMap.put("status", "success");
         } catch (Exception e) {
             System.out.println("Nie udało się podłączyć zewnętrzej apoliacji!!!!");
             e.getMessage();
             myMap.put("status", "faild");
         }
-        return myMap;
-    }
+       return myMap;
+   }
 }
