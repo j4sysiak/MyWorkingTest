6c4f2f7ecd149d0bdd849ae671f97393b12f25ba
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-16 18:52:21 +0200

    16-6-2022 17:11

--- springboot-metrics/src/main/java/com/example/demo/TestConnector.java
+++ springboot-metrics/src/main/java/com/example/demo/TestConnector.java
@@ -2,13 +2,34 @@ package com.example.demo;
 
 
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Endpoint(id="test-connector")
 public class TestConnector {
 
-    public String get() {
-        return "";
+    @ReadOperation
+    public Map<String, String> get() {
+        Map<String, String> myMap = new HashMap();
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.exchange("http://localhost:8081/test1", HttpMethod.GET, HttpEntity.EMPTY, String.class);
+            //jeżeli uda się połączyć zewnętrzną aplikację - to zwracamy success
+            //uruchamiamy zewnętrzy Rest: apka jest w: c:\Users\Jacek\Documents\JAVA\SpringBoot\MyWorkingTest\   działa na porcie 8081
+            //
+            myMap.put("status", "success");
+        } catch (Exception e) {
+            System.out.println("Nie udało się podłączyć zewnętrzej apoliacji!!!!");
+            e.getMessage();
+            myMap.put("status", "faild");
+        }
+        return myMap;
     }
 }
