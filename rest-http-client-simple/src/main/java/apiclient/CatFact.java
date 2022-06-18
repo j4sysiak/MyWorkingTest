b10854e80211bc48471a1b598f925176b7cae364
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-18 09:11:55 +0200

    18-06-2022 06:02

--- rest-http-client-simple/src/main/java/apiclient/CatFact.java
+++ rest-http-client-simple/src/main/java/apiclient/CatFact.java
@@ -1,4 +1,4 @@
-package test;
+package apiclient;
 
 import javax.annotation.processing.Generated;
 
---
+++ rest-http-client-simple/src/main/java/apiclient/RestClient.java
@@ -0,0 +1,73 @@
+package apiclient;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class RestClient {
+
+    public static void main(String [] args) throws IOException {
+
+        // pobieramy jsona z jakiegoś zewnętrznego api  (lista różnych publicznych API: https://github.com/public-apis/public-apis)
+        URL url = new URL("https://cat-fact.herokuapp.com/facts/random");
+        InputStreamReader inputStreamReader = new InputStreamReader(url.openStream());
+
+        //Generator klas na podstawie jsona
+        //korzystamy z https://www.jsonschema2pojo.org
+        //narzędzie to generuje klasy na podstawie Jsona
+        //Tworzymy klasę CatFact oraz do Status
+        //teraz będzie można utworzyć cały obiekt CatFact z zaciągniętego zewnętrznego Jsona
+        CatFact catFact = new Gson().fromJson(inputStreamReader, CatFact.class);
+
+        System.out.println(catFact.getText());
+
+        // lub
+
+        URL url1 = new URL("https://aws.random.cat/meow");
+        InputStreamReader inputStreamReader1 = new InputStreamReader(url1.openStream());
+        JsonObject jsonObject = new JsonParser().parse(inputStreamReader1).getAsJsonObject();
+        String randomCatFile = jsonObject.get("file").getAsString();
+        System.out.println(randomCatFile);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
