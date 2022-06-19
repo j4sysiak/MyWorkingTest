e48d175d5c3b337367b1a855f0db89a16f38c840
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-19 05:33:24 +0200

    19-06-2022 05:33

---
+++ rest-http-client-spotify/src/main/java/com/example/demo/UserInfo.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserInfo {
+
+    @GetMapping("/user")
+    public Principal get(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/login")
+    public Principal login(Principal principal) {
+        return principal;
+    }
+}
