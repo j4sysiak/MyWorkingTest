aee0dbd27a33aa2ceb71ec8777d39ffbc843454d
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 18:12:16 +0200

    22-06-2022 17:55

---
+++ rectangle-hibernate-vaadin-app/Readme.txt
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=Ie5dBCBDIBo&list=PLUtcRmGoaP27yn8YOgczKcXJgmvAq6EsE
+
+odpalamy:  http://localhost:8080/rectanglegui
+
+
--- rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/gui/RectangleGui.java
+++ rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/gui/RectangleGui.java
@@ -1,12 +1,27 @@
 package com.example.demo.gui;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+
+// odpalamy tą stronę:  http://localhost:8080/rectanglegui
 
 @Route
 public class RectangleGui extends VerticalLayout {
 
     private TextField textFieldHeight;
     private TextField textFieldWeight;
+    private Button button;
+
+    public RectangleGui() {
+        this.textFieldHeight = new TextField("Podaj wysokość: ");
+        this.textFieldWeight = new TextField("Podaj szerokość: ");
+        this.button = new Button("Dodaj");
+
+        add(textFieldHeight);
+        add(textFieldWeight);
+        add(button);
+    }
+
 }
