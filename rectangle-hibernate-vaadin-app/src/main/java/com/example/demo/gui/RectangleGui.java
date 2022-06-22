8c6b2d3ff54476c50068caa23692a3301e0cce36
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 18:05:35 +0200

    22-06-2022 17:55

--- rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/gui/RectangleGui.java
+++ rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/gui/RectangleGui.java
@@ -1,8 +1,12 @@
 package com.example.demo.gui;
 
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route
-public class RectangleGui {
+public class RectangleGui extends VerticalLayout {
 
+    private TextField textFieldHeight;
+    private TextField textFieldWeight;
 }
