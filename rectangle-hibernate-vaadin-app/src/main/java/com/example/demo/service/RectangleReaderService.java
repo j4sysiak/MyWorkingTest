063add2303632d5b9fe968c71d14715d58f17283
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 19:27:14 +0200

    22-06-2022 17:55

--- rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/service/RectangleReaderService.java
+++ rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/service/RectangleReaderService.java
@@ -1,29 +1,73 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Rectangle;
+import com.example.demo.repository.RectangleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RectangleReaderService {
 
+    // Żeby zapisać Rectangle w bazie to musimy wstrzyknąć beana RectangleRepo
+    private RectangleRepo rectangleRepo;
+
+    @Autowired
+    public RectangleReaderService(RectangleRepo rectangleRepo) {
+        this.rectangleRepo = rectangleRepo;
+    }
+
     //wstrzyknięcie wartości z konfiguracji
-    @Value("{siteA1}")
+    @Value("${siteA1}")
     private int siteA1;
-    @Value("{siteB1}")
+    @Value("${siteB1}")
     private int siteB1;
 
-    @Value("{siteA2}")
+    @Value("${siteA2}")
     private int siteA2;
-    @Value("{siteB2}")
+    @Value("${siteB2}")
     private int siteB2;
 
-    @Value("{siteA3}")
+    @Value("${siteA3}")
     private int siteA3;
-    @Value("{siteB3}")
+    @Value("${siteB3}")
     private int siteB3;
 
-    @Value("{siteA4}")
+    @Value("${siteA4}")
     private int siteA4;
-    @Value("{siteB4}")
+    @Value("${siteB4}")
     private int siteB4;
+
+    // znaczenie tej adnotacji: zdarzenie ma być przechwycone przez aspect (Beffore...    ...after)
+    // np. w momenncie startu aplikacji włazimy tutaj i budujemy cztery prostokąty
+    @EventListener(ApplicationReadyEvent.class)
+    public void addRectangles() {
+        Rectangle r1 = Rectangle.builder()
+                .height(siteA1)
+                .weight(siteB1)
+                .build();
+
+        Rectangle r2 = Rectangle.builder()
+                .height(siteA2)
+                .weight(siteB3)
+                .build();
+
+        Rectangle r3 = Rectangle.builder()
+                .height(siteA3)
+                .weight(siteB3)
+                .build();
+
+        Rectangle r4 = Rectangle.builder()
+                .height(siteA4)
+                .weight(siteB4)
+                .build();
+
+        System.out.println("Zapis tych elementó do bazy");
+        rectangleRepo.save(r1);
+        rectangleRepo.save(r2);
+        rectangleRepo.save(r3);
+        rectangleRepo.save(r4);
+    }
 }
