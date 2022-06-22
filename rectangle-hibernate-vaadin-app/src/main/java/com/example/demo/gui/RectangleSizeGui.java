ef82a7facda5b6d7b230dedbd687eb80e70659d4
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 20:03:42 +0200

    22-06-2022 17:55

--- rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/gui/RectangleSizeGui.java
+++ rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/gui/RectangleSizeGui.java
@@ -1,35 +1,42 @@
 package com.example.demo.gui;
 
-import com.example.demo.model.Rectangle;
 import com.example.demo.repository.RectangleRepo;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-
 @Route
-public class RectangleSizeGui  extends VerticalLayout {
+public class RectangleSizeGui extends VerticalLayout {
 
     //wstrzykujemy repo - żeby coś z bazą zrobić
     private RectangleRepo rectangleRepo;
 
     private TextArea textAreaRectangles;
     private TextField textFieldSize;
+    private Button buttonBigger;
+    private Button buttonSmaller;
 
     @Autowired
     public RectangleSizeGui(RectangleRepo rectangleRepo) {
         this.rectangleRepo = rectangleRepo;
-        this.textFieldSize = new TextField();
-        this.textAreaRectangles = new TextArea();
+        this.textFieldSize = new TextField("Podaj rozmiar: ");
+        this.textAreaRectangles = new TextArea("Wynik: ");
+        this.buttonBigger = new Button("Pokaż większe");
+        this.buttonSmaller = new Button("Pokaż mniejsze");
 
-        textAreaRectangles.setValue(rectangleRepo.getListOfBigRectangles(Integer.parseInt(textFieldSize.getValue())).toString());
+        buttonBigger.addClickListener(buttonClickEvent -> rectangleRepo
+                .getListOfBigRectangles(Integer.parseInt(textFieldSize.getValue())).toString());
+
+        buttonSmaller.addClickListener(buttonClickEvent -> rectangleRepo
+                .getListOfSmallRectangles(Integer.parseInt(textFieldSize.getValue())).toString());
+
         add(textAreaRectangles);
         add(textFieldSize);
+        add(buttonBigger);
+        add(buttonSmaller);
     }
-
-
 
 }
