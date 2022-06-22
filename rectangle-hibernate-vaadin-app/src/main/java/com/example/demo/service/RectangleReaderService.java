bf3d4654d9dd2729a1e1675484bf20e7ec9859f2
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 19:15:22 +0200

    22-06-2022 17:55

--- rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/service/RectangleReaderService.java
+++ rectangle-hibernate-vaadin-app/src/main/java/com/example/demo/service/RectangleReaderService.java
@@ -9,25 +9,21 @@ public class RectangleReaderService {
     //wstrzyknięcie wartości z konfiguracji
     @Value("{siteA1}")
     private int siteA1;
-
     @Value("{siteB1}")
     private int siteB1;
 
     @Value("{siteA2}")
     private int siteA2;
-
     @Value("{siteB2}")
     private int siteB2;
 
     @Value("{siteA3}")
     private int siteA3;
-
     @Value("{siteB3}")
     private int siteB3;
 
     @Value("{siteA4}")
     private int siteA4;
-
     @Value("{siteB4}")
     private int siteB4;
 }
